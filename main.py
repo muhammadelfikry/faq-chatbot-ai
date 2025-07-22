2ad164755dcc83781480353753c2e708b39c14d0
Muhammad Elfikry <melfikry@gmail.com>
2025-07-22 17:28:58 +0700

    feat(cors): add CORS middleware configuration

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from groq import Groq
 from dotenv import load_dotenv
@@ -6,10 +7,17 @@ import os
 
 load_dotenv()
 
-app = FastAPI()
-
 client = Groq(
     api_key=os.getenv("GRQQ_API_KEY")
+)
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 with open("prompt.txt", "r") as f:
@@ -46,3 +54,9 @@ def chat_bot(req: ChatRequest):
 
     except Exception as e:
         return {"error": str(e)}
+    
+
+if __name__ == "__main__":
+    import uvicorn
+    
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
